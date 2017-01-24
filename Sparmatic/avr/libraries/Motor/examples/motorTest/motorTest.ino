d1f6ac3abd0dc6afa0eaac4b79d644d2b92b3ef7
Stefan M <root@geeks-r-us.de>
2017-01-25 00:53:35 +0100

    striepd down motor to minimum example

--- Sparmatic/avr/libraries/Motor/examples/motorTest/motorTest.ino
+++ Sparmatic/avr/libraries/Motor/examples/motorTest/motorTest.ino
@@ -1,11 +1,45 @@
 #include <motor.h>
+#include <input.h>
+#include <lcd.h>
 
 void setup()
 {
-    Motor.adapt();
+	LCD.init();
+	Input.init();
+	Motor.init();	
 }
 
 void loop()
 {
+    int8_t value = Input.encoderRead();
+	LCD.displayNumber(value, 4);
+	Motor.move((enum MotorDir)value);
+	delay(1000);
+}
+
+ISR(LCD_vect)
+{
+    Input.periodicScan();
     
+}
+
+ISR(PCINT1_vect)
+{
+	Input.periodicScan();
+}
+
+
+#define PCINT0_PORTIN PINE
+ISR(PCINT0_vect)
+{
+	static unsigned char lastState = 0; // init to defaults
+	unsigned char newState = PCINT0_PORTIN;
+	unsigned char changed = newState ^ lastState;
+	lastState = newState;
+	
+	// motor step
+	if(changed & (1 << MOTOR_SENSE_PIN))
+	{
+		Motor.step();
+	}
 }
--- Sparmatic/avr/libraries/Motor/src/motor.h
+++ Sparmatic/avr/libraries/Motor/src/motor.h
@@ -1,15 +1,3 @@
-/// \file motor.h
-/// 
-/// 
-/// 
-
-/*
- * motor.h
- *
- *  Created on: 19.11.2011
- *      Author: matthias
- */
-
 #ifndef MOTOR_H_
 #define MOTOR_H_
 
@@ -17,38 +5,30 @@
 
 #define MOTOR_SENSE_PIN PE1
 
+enum MotorDir {
+	CLOSE = -1,
+	STOP = 0,
+	OPEN = 1
+};
+
 
 class motor {
 public:    
     motor();
     void init();
-    void stepOpen();
-    void stepClose();
-    uint8_t step();
-    void moveTo(uint8_t valve);
-    uint8_t fullOpen();
-    uint8_t adapt();
-    uint8_t isRunning();
-    void timer();
-
+	void move(enum MotorDir direction);
 private:
-    uint16_t getCurrent();
-    void stopMove();
-    void stopTimeout();
-    void move(int8_t dir);
-
-
-    int16_t _position;
-    int16_t _positionValveOpen;
-    int16_t _positionValveClosed;
-    uint8_t _stopSource;
-    int8_t _direction;
-    int16_t _targetPosition;
-    uint8_t _timeout;
-    uint16_t _currentLimit;
+	void open();
+	void close();
+	void stop();
+	
+    
+private:
+	enum MotorDir m_direction;
 };
 
 static motor Motor;
 #define getMotorPosition(x) ((const volatile int16_t)MotorPosition)
 
 #endif /* MOTOR_H_ */
+
